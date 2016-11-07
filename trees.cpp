7357ad7cd42f47d1f0119974ac16babedeefc99d
Garend95 <garend95@yahoo.com>
2016-11-07 20:55:08 +0400

    remove function commented due to errors, slight changes to tree class,choice input in main.

--- trees.cpp
+++ trees.cpp
@@ -15,11 +15,18 @@ class tree {
 		void insert(int data);
 		void insertFirst();
 		void insertHelper(Node* &subTree, Node* newNode);
-		void remove();
+		void remove(int data, Node* startingPoint = NULL);
 		bool isEmpty();
 		int getHeight();
 		int find(); 
-		tree();
+		Node* getRoot()
+		{
+		    return root;
+		}
+		tree()
+        {
+        	root = NULL;
+        }
 };
 
 void tree::insertHelper(Node* &subTree, Node* newNode){
@@ -46,21 +53,67 @@ void tree::insert(int data){
 }
 
 
-void tree::remove(int data, Node* startingPoint = root){
-	if(startingPoint != Null)
+/*void tree::remove(int data, Node* startingPoint = NULL){
+    if(startingPoint == NULL) startingPoint = root;	
+    
+    if(startingPoint->data == data) startingPoint = NULL;
+	/*if(startingPoint != Null)
 	{
-		if (startingPoint->data)
-	}
+		if (startingPoint->data == data) ~startingPoint;
+	}*/
 
-}
+//}
 
-tree::tree()
-{
-	root = NULL;
-}
+
 
 int main(){
-	cout << "Turn down for what!?" << endl;
+    
+    tree tr;
+    
+    int choice;
+    
+    do{
+        cout << "What do you want to do?" << endl
+             << "1-insert" << endl << "2-remove" << endl << "3-find" << endl
+             << "4-inorder" << endl << "5-preorder" << endl << "6-postorder" << endl
+             << "7-get height" << endl << "0-exit";
+        
+        cin >> choice;
+        
+        int member;
+        
+        switch (choice) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                
+                break;
+            case 0:
+                
+                break;    
+        }
+    
+        
+    }while(choice != 0)  
+         
+    //tr.insert(5);
+	//cout << "Turn down for what!?" << endl;
 	return 0;
 	
 }
